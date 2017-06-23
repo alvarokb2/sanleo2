419e81e0a9d291a3ce1c7d7bf49dfd441a117763
belaly <gladys.bta@gmail.com>
2017-06-22 20:16:45 -0400

    agregado ver alumnos al curso

--- resources/views/educadora/cursos/cursos.blade.php
+++ resources/views/educadora/cursos/cursos.blade.php
@@ -3,7 +3,4 @@
 @section('contenido')
     @include('educadora.partials.lista_cursos')
 
-    <div class="container-fluid">
-        <a class="btn btn-primary" href="{{route('curso.create')}}">Nuevo</a>
-    </div>
 @endsection
