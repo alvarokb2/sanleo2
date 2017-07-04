7703f9e35b42b2e833dcb42b9928df859a26d68e
belaly <gladys.bta@gmail.com>
2017-07-04 10:38:08 -0400

    nose que hice

--- resources/views/partials/lista_subareas.blade.php
+++ resources/views/partials/lista_subareas.blade.php
@@ -1,8 +1,8 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-1">
         </div>
-        <div class="col-md-8">
+        <div class="col-md-11">
            <table class="table table-condensed  table-bordered">
                 <thead>
                 <tr>
